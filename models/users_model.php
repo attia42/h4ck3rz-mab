640c8ee22d667a4512610ce5648938a929a35c8c
mail.gamal <mail.gamal@e9a01bb5-3751-0410-b27c-696006922e5c>
2008-07-16 16:20:59 +0000

    I added a demo users model file I added some convention for file naming and methods documentation feed back please note that I don't like the model _base class !!

---
+++ models/users_model.php
@@ -0,0 +1,36 @@
+<?php
+//ma3a enne msh moQtane3 tamaman b model_base class .. ya snap !!
+// ----------------------------------------------------------------------
+// Class Name: 				[demo]users Model Class
+//Class:							Users
+// Filename:   				users_model.php
+// Original Author(s):		<gamal>El-Sayed G. Abdul-Azem [CAT-Hackers MyAdressBook Project] <mail.gamal@gmail.com>
+// Purpose:   				Basic CRUD functions Authentication and last user method
+// ----------------------------------------------------------------------
+
+
+require_once("../classes/model_base.php");
+
+class Users extends Model
+{
+ /**
+    * @return mixed valid userdata or false
+    * @param String $username  valedated input username
+								$passwd  valedated input password
+    *        
+    * @desc  Authenticated single user from the `users` table
+    * @El-Sayed G. Abdul-Azem
+    */
+	function auth($username,$passwd)
+	{
+			//$queryArray["selections"]=array();
+			$queryUserArray['tables']=array('users');
+			$queryUserArray["whereCondition"]="`username`='$username' AND `password` = '$password'  AND  `Disabled` <> 1";
+			$user=$this->BuildSqlSelect($queryUserArray);
+			if ($userData=$this->Query($user))
+			{
+					return $userData;
+			}
+			return false;
+	}
+?>
