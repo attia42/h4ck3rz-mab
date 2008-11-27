f4a4b58fbd27054ffd3951dac61722d8c3f7ef6c
clever.netman <clever.netman@e9a01bb5-3751-0410-b27c-696006922e5c>
2008-11-27 20:28:19 +0000

    Moved Project to trunk/

--- views/contactgroup/viewgroups/row.php
+++
@@ -1,6 +0,0 @@
-<tr>
-		<td><input name="{{id}}" type="checkbox" /></td>
-		<td><img src="{{avatar}}" width="100" alt=""/>&nbsp</td>
-		<td>&nbsp<a href="../contacts/phonebook?group={{id}}">{{name}}</a></td>
-		<td><a href="../contactgroup/editgroup?id={{id}}">Edit</a></td>
-</tr>
